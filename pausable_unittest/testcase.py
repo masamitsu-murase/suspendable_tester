1b8ae6a0a6d0771a2e5f9ab268c887211b64033e
Masamitsu MURASE <masamitsu.murase@gmail.com>
2016-06-02 08:08:22 +0900

    Restore directory after resuming.

--- pausable_unittest/testcase.py
+++ pausable_unittest/testcase.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 
 import unittest
+import os
 
 class TestCase(unittest.TestCase):
     def run(self, result):
@@ -11,8 +12,23 @@ class TestCase(unittest.TestCase):
 
     def pause(self, info=None):
         self.__result.before_pause(info)
+        status = self.extra_status()
+
         self.__pause_forwarder.pause(info)
+
+        self.restore_extra_status(status)
         self.__result.after_pause(info)
+
+    def extra_status(self):
+        status = {}
+        status["cwd"] = os.path.abspath(os.getcwd())
+        return status
+
+    def restore_extra_status(self, status):
+        try:
+            os.chdir(status["cwd"])
+        except:
+            self.logger.error("Cannot change directory to '%s'.", status["cwd"])
 
     @property
     def logger(self):

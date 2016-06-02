8a19487abad59c33e5f743de27b7ebdb57c8511a
Masamitsu MURASE <masamitsu.murase@gmail.com>
2016-06-03 07:54:51 +0900

    Add test for chdir.

--- test/test.sh
+++ test/test.sh
@@ -1,17 +1,13 @@
 
-# Change current directory.
-# This script must be called from the root directory.
-cd test
-
-rm -f test_result.txt teststate.bin
+rm -f test/test_result.txt teststate.bin
 
 for i in {0..4}
 do
-    pypy --jit off test.py | tee -a test_result.txt
+    pypy --jit off test/test.py | tee -a test/test_result.txt
     sleep 1
 done
 
-pypy --jit off check_result.py
+pypy --jit off test/check_result.py
 
 echo ""
 echo OK
